bf3663c3ca344815f74c6b571c20d4ab2b16ae90
Sumanth Chinthagunta <xmlking@gmail.com>
2023-01-29 13:19:09 -0800

    feat: add houdini project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,1 @@
- <script lang="ts">
-    import { graphql } from '$houdini';
-      const store = graphql(`
-        query sample_poke_query @load {
-          pokemon_v2_pokemon(limit: 20) {
-            id
-            name
-            height
-            weight
-          }
-        }
-      `);
-</script>
-  
-  
-<pre>{JSON.stringify($store, null, 2)}</pre>
-  
+<a href="./test1" >test1</a>
---
+++ src/routes/test1/+page.svelte
@@ -0,0 +1,17 @@
+ <script lang="ts">
+    import { graphql } from '$houdini';
+      const store = graphql(`
+        query sample_poke_query @load {
+          pokemon_v2_pokemon(limit: 20) {
+            id
+            name
+            height
+            weight
+          }
+        }
+      `);
+</script>
+  
+  
+<pre>{JSON.stringify($store, null, 2)}</pre>
+  
